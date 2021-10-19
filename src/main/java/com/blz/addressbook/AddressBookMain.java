907a441bd07a01dd7cd914871a681a898da4f077
Saurabh Siddharth <saurabhsiddharth68>
2021-10-19 14:07:34 +0530

    Added:Address Book

---
+++ src/main/java/com/blz/addressbook/AddressBookMain.java
@@ -0,0 +1,19 @@
+package com.blz.addressbook;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        Scanner scanner = new Scanner(System.in);
+        addressBook.addContact();
+        System.out.println("Enter 1 To Edit The Contact and 2 To Delete The Contact ");
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            addressBook.editContact();
+        } else {
+            addressBook.deleteContact();
+        }
+    }
+}
