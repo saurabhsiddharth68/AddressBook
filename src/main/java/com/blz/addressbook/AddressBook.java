64baf45287ad729d0c2744045f603865ae264aa8
Saurabh Siddharth <saurabhsiddharth68>
2021-11-10 02:05:26 +0530

    Added: Added Count Person

--- src/main/java/com/blz/addressbook/AddressBook.java
+++ src/main/java/com/blz/addressbook/AddressBook.java
@@ -76,6 +76,21 @@ public class AddressBook {
     }
 
     /*
+    Declaring The Count Contacts Method By City Name
+    Using Java Streams To Count The Contacts By using City Name
+    */
+    public void countContactsByUsingCity(String cityName) {
+        long count = 0;
+        long count1 = contactDetailsList.stream().filter(g -> g.getCity().equalsIgnoreCase(cityName)).count();
+        for (ContactDetails contact : contactDetailsList) {
+            count1 = count1 + count;
+        }
+        System.out.println("Contact List :" + count1);
+
+    }
+
+
+    /*
     Declaring The Edit Contact Method
     TO Edit The Details Of Contact
     The Details Of Contact Edit By Using FirstName
@@ -181,7 +196,7 @@ public class AddressBook {
     public void viewByOptions() {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            System.out.println("Enter\n 1. By name\n 2. By city\n 3. By state\n 4. for previous menu");
+            System.out.println("Enter\n 1. By name\n 2. By city\n 3. By state\n 4. Count Contacts\n 0. for previous menu");
             int choice = scanner.nextInt();
             scanner.nextLine();
             switch (choice) {
@@ -198,9 +213,13 @@ public class AddressBook {
                 case 3:
                     System.out.println("Enter state: ");
                     String state = scanner.nextLine();
-                    System.out.println(state);
+                    searchByState(state);
                     break;
                 case 4:
+                    System.out.println("Enter The Name Of City");
+                    String cityName = scanner.next();
+                    countContactsByUsingCity(cityName);
+                case 0:
                     return;
                 default:
                     System.out.println("Entered choice is incorrect!.. please enter correct choice");
